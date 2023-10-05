180a0e7c8fcbbe917a800260b08176d9fea29164
Lucas Pinheiro <lucasferreirapinheiro@gmail.com>
2023-10-04 23:12:10 -0300

    Corrigidos os graficos de pareto e adicionada condicao de parada

--- pyomo/main.py
+++ pyomo/main.py
@@ -13,7 +13,7 @@ import time
 import sys
 
 #Constante que determina o percentual minimo da cobertura maxima a ser alcancado. Condicao de parada.
-stop_perc = 0.90
+stop_perc = 0.50
 
 # Zera o tempo decorrido total
 tt0 = time.time()
@@ -63,9 +63,12 @@ for L in range(10, 15, 5):
         eps_MAX = preproc_C_max(instance1)
         ttt = time.time() - ttt0
 
-        eps_MIN = 0
+        #Reduz o epsilon maximo a 90% da cobertura maxima
+        eps_MAX = floor(stop_perc * eps_MAX)
 
-        eps_step = floor(eps_MAX/2)
+        eps_step = floor(eps_MAX/10)
+
+        eps_MIN = eps_step
 
         #Inicializa variavel de execucao adicional para o ultimo epsilon
         ad_run = False
@@ -78,11 +81,11 @@ for L in range(10, 15, 5):
             
             #Se esta na run adicional, epsilon atual deve ser o epsilon maximo. Programa encerramento atualizando o valor de epsilon maximo original.
             if(ad_run):
-                eps = eps_bkp
                 eps_MAX = eps_bkp
+                eps = eps_MAX
 
             #Se o proximo passo ultrapassar o maximo, precisa de uma nova execucao para eps_MAX
-            if(eps + eps_step > eps_MAX):
+            if((eps + eps_step > eps_MAX) and eps != eps_MAX):
                 eps_bkp = eps_MAX
                 eps_MAX = eps_MAX*1000
                 ad_run = True
@@ -120,19 +123,12 @@ for L in range(10, 15, 5):
 
             # Cria um solver
             opt = SolverFactory(solver, executable=solver_exec)
-            # opt.options["tmlim"] = 1200
+            opt.options["tmlim"] = 36000
             opt.options["mipgap"] = 0.01
 
             print("Translating instance to solver...\n")
             # Resolve a instancia e armazena os resultados em um arquivo JSON
-            results, presolve_time = opt.solve(instance, tee=True)
-
-            # Adiciona log do tempo do Pyomo
-            # f = open("./output/logs/" + str(L) + "x" + str(L) + "/d0." + str(d) + "/" + figname + "_timing.txt","w")
-
-            #timing.report_timing(f, stream=True, level=20)
-            
-            print("\nTook "+ str(presolve_time) + " s to build and translate to solver.\n")
+            results = opt.solve(instance, tee=True)
 
             instance.solutions.store_to(results)
             results.problem.name = instance_filename
@@ -158,7 +154,7 @@ for L in range(10, 15, 5):
             # Funcao objetivo de Energia (valor otimo)
             E_now = value(instance.E)
             sol_E.append(E_now)
-            print("E = " + str(E_now) + "mA")
+            print("E = " + str(E_now) + " mA")
 
             # Variavel de decisao da Cobertura (valor otimo)
             C_now = value(instance.objC)
@@ -302,252 +298,12 @@ for L in range(10, 15, 5):
 
             eps = eps + eps_step
 
-            ###Condicao de parada! Se alcancou pelo menos X% da cobertura maxima.
-            # if(C_now >= stop_perc * eps_MAX):
-                    
-            #     print("Stopped by epsilon. " + str(C_now) + " >= 90% * " + str(eps_MAX))
-            #     sys.stdout.close()
-            #     break
-
-    
-    # #Executa rodada adicional apenas para o ultimo epsilon, caso ja nao tenha sido considerado
-    # if(ad_run):
-
-    #     eps = eps_MAX
-
-    #     # Zera o tempo decorrido no epsilon
-    #     t0 = time.time()
-
-    #     # Nome do arquivo da instancia a ser resolvida
-
-    #     instance_eps = "_eps" + str(eps)
-
-    #     # Define nome da figura a salvar
-    #     figname = instance_filename[:-4] + instance_eps
-
-    #     # Nome do arquivo de log
-    #     sys.stdout = open("./output/logs/" + str(L) + "x" + str(L) + "/d0." + str(d) + "/" + figname + ".txt","w")
-
-    #     ## CONSTRUCAO DO MODELO E INSTANCIA
-
-    #     ## Gera modelo abstrato com epsilon correspondente
-    #     model = generate_model(eps)
-
-    #     ## Carrega dados de instancia no modelo
-    #     data = DataPortal()
-    #     data.load(filename=instance_path + instance_filename, model=model)
-    #     instance = model.create_instance(data)
-
-    #     # instance.epsC.pprint()
-    #     # instance.objC.pprint()
-    #     # continue
-    #     # -----------------------------------------------------------#
-
-    #     ## SOLVER
-    #     ## ------
-
-    #     # Cria um solver
-    #     opt = SolverFactory(solver, executable=solver_exec)
-    #     # opt.options["tmlim"] = 1200
-    #     opt.options["mipgap"] = 0.0001
-
-    #     print("Translating instance to solver...\n")
-    #     # Resolve a instancia e armazena os resultados em um arquivo JSON
-    #     results = opt.solve(instance, tee=True)
-    #     instance.solutions.store_to(results)
-    #     results.problem.name = instance_filename
-    #     results.write(filename="results.json", format="json")
-
-    #     if results.solver.termination_condition == "maxTimeLimit":
-    #         print("\n\nSOLVER TIME LIMIT EXCEEDED\n\n")
-    #         if math.isinf(results.problem.lower_bound) or math.isinf(
-    #             results.problem.upper_bound
-    #         ):
-    #             print("\n\nNO SOLUTION FOUND FOR THIS INSTANCE!\n\n")
-    #             continue
-
-    #     # Pega resultados diretamente
-    #     print("\nResults:\n")
-
-    #     # PEGAR RESULTADOS:
-    #     # VALOR DA FO DE ENERGIA
-    #     # VALOR DA RESTRICAO DE COBERTURA (FO DE COBERTURA)
-    #     # TEMPO DE EXECUCAO (DO SOLVER)
-    #     # GAP DE OTIMALIDADE
-    #     #
-    #     # Funcao objetivo de Energia (valor otimo)
-    #     E_now = value(instance.E)
-    #     sol_E.append(E_now)
-    #     print("E = " + str(E_now) + "mA")
-
-    #     # Variavel de decisao da Cobertura (valor otimo)
-    #     C_now = value(instance.objC)
-    #     sol_C.append(C_now)
-    #     print("C = " + str(C_now) + "points")
-    #     print("C ~= " + str(((C_now-2)/2)*1.6) + "km^2")
-
-    #     # Valor de epsilon utilizado nessa execucao
-    #     sol_eps.append(eps)
-    #     print("Minimum Epsilon for this instance = " + str(eps_MIN))
-    #     print("Maximum Epsilon for this instance = " + str(eps_MAX))
-    #     print("Current Epsilon = " + str(eps))
-
-    #     sol_time.append(results.solver.time)
-    #     # Tempo de execucao total (incluido tempo de traducao do modelo do pyomo para o solver)
-    #     print("Time in solver (for this epsilon): " + str(results.solver.time) + " s")
-        
-    #     t = time.time() - t0
-    #     print("Elapsed time (for this epsilon): " + str(t))
-
-    #     tt = time.time() - tt0
-    #     print("\n\nTotal elapsed time since execution of first epsilon: " + str(tt))
-
-    #     # -----------------------------------------------------------#
-
-    #     ## POS-PROCESSAMENTO
-    #     ## -----------------
-
-    #     # Plota todos os espacos disponiveis para alocacao como circulos nao-preenchidos pretos
-    #     # Se estiver alocado, preenche o circulo e colore de acordo com modelo de transceptor ativo
-
-    #     fig = plt.figure("SAP Optimal Result")
-    #     ax = fig.add_subplot(1, 1, 1)
-    #     ax.axis("equal")
-
-    #     for i in instance.V:
-    #         plt.text(
-    #             (instance.X[i]-1 + 0.1)*instance.scale,
-    #             (instance.Y[i]-1 + 0.1)*instance.scale,
-    #             str(i),
-    #             color="k",
-    #             fontsize=10,
-    #         )
-
-    #         if value(instance.s["S2C", i]) == 1:
-    #             ax.plot((instance.X[i]-1)*instance.scale, (instance.Y[i]-1)*instance.scale, "go")
-    #             ax.add_patch(
-    #                 plt.Circle(
-    #                     ((instance.X[i]-1)*instance.scale, (instance.Y[i]-1)*instance.scale),
-    #                     (instance.RMAX["S2C"]),
-    #                     color="g",
-    #                     alpha=0.1,
-    #                 )
-    #             )
-    #         elif value(instance.s["S2CPro", i]) == 1:
-    #             ax.plot((instance.X[i]-1)*instance.scale, (instance.Y[i]-1)*instance.scale, "bo")
-    #             ax.add_patch(
-    #                 plt.Circle(
-    #                     ((instance.X[i]-1)*instance.scale, (instance.Y[i]-1)*instance.scale),
-    #                     (instance.RMAX["S2CPro"]),
-    #                     color="b",
-    #                     alpha=0.1,
-    #                 )
-    #             )
-    #         elif value(instance.s["S3", i]) == 1:
-    #             ax.plot((instance.X[i]-1)*instance.scale, (instance.Y[i]-1)*instance.scale, "ro")
-    #             ax.add_patch(
-    #                 plt.Circle(
-    #                     ((instance.X[i]-1)*instance.scale, (instance.Y[i]-1)*instance.scale),
-    #                     (instance.RMAX["S3"]),
-    #                     color="r",
-    #                     alpha=0.1,
-    #                 )
-    #             )
-    #         else:
-    #             ax.plot((instance.X[i]-1)*instance.scale, (instance.Y[i]-1)*instance.scale, "ko", fillstyle="none")
-
-    #     ax.grid(linestyle="--", linewidth=0.5, alpha=0.5)
-    #     ax.set_xticks(np.arange(instance.W[1]*instance.scale,instance.W[instance.dimW]*instance.scale))
-    #     ax.set_yticks(np.arange(instance.H[1]*instance.scale,instance.H[instance.dimH]*instance.scale))
-
-    #     red_patch = mpatches.Patch(color="red", label="S3Pro")
-    #     blue_patch = mpatches.Patch(color="blue", label="S2CPro")
-    #     green_patch = mpatches.Patch(color="green", label="S2C")
-
-    #     ax.legend(handles=[red_patch, blue_patch, green_patch], loc="upper right")
-
-    #     plt.plot(
-    #         [instance.W[1]*instance.scale, instance.W[instance.dimW]*instance.scale],
-    #         [instance.H[1]*instance.scale, instance.H[1]*instance.scale],
-    #         color="k",
-    #     )
-    #     plt.plot(
-    #         [instance.W[instance.dimW]*instance.scale, instance.W[instance.dimW]*instance.scale],
-    #         [instance.H[1]*instance.scale, instance.H[instance.dimH]*instance.scale],
-    #         color="k",
-    #     )
-    #     plt.plot(
-    #         [instance.W[1]*instance.scale, instance.W[1]*instance.scale],
-    #         [instance.H[1]*instance.scale, instance.H[instance.dimH]*instance.scale],
-    #         color="k",
-    #     )
-    #     plt.plot(
-    #         [instance.W[1]*instance.scale, instance.W[instance.dimW]*instance.scale],
-    #         [instance.H[instance.dimH]*instance.scale, instance.H[instance.dimH]*instance.scale],
-    #         color="k",
-    #     )
-
-    #     # for i in instance.KW:
-    #     #     for j in instance.KH:
-    #     #         if value(instance.cc[i, j]):
-    #     #             plt.plot(
-    #     #                 instance.W[i],
-    #     #                 instance.H[j],
-    #     #                 marker="x",
-    #     #                 color="r",
-    #     #                 alpha=0.3,
-    #     #             )
-    #     #         else:
-    #     #             plt.plot(
-    #     #                 instance.W[i],
-    #     #                 instance.H[j],
-    #     #                 marker="x",
-    #     #                 color="k",
-    #     #                 alpha=0.3,
-    #     #             )
-
-    #     plt.xlabel("X Coordinates (km)")
-    #     plt.ylabel("Y Coordinates (km)")
-    #     # plt.title('Instance: '+str(instance_filename)+'\nScale: 1:'+str(int(instance.scale))+'m\nAlphas: '+str(alpha['E'])+'E, '+str(alpha['C'])+'C, '+str(alpha['M'])+'M  -  Time: '+str(results.solver.user_time)+' s')
-    #     # plt.show()
-    #     plt.savefig("./output/" + str(L) + "x" + str(L) + "/d0." + str(d) + "/" + figname +".svg")
-    #     plt.close()
-
-    #     sys.stdout.close()
 
     sys.stdout = open("./output/logs/" + str(L) + "x" + str(L) + "/d0." + str(d) + "/" + instance_filename[:-4] + "_pareto.txt","w")
     # sys.stdout = sys.__stdout__
     
     try:
         # Plotando a fronteira de pareto
-        # print("Checking if x is strictly increasing sequence...")
-
-        # print("\nBefore (E): " + str(sol_E))
-        # print("\nBefore (C): " + str(sol_C))
-
-        # x1 = 0
-        # x2 = 1
-
-        # new_sol_E = sol_E
-        # new_sol_C = sol_C
-
-        # print("\n")
-
-        # for x in range(0,len(sol_E)-3):
-            
-        #     # print("\nX1: "+ str(sol_E[x1]))
-        #     # print("\nX2: "+ str(sol_E[x2]))
-
-        #     if sol_E[x1] == sol_E[x2]:
-        #         del new_sol_E[x1]
-        #         del new_sol_C[x1]
-            
-        #     x1 += 1
-        #     x2 += 1
-            
-        # print("\nAfter (E): " + str(new_sol_E))
-        # print("\nAfter (C): " + str(new_sol_C))
-
         print("Started to plot Pareto Frontier...\n")
 
         fig = plt.figure("Pareto Frontier")
@@ -568,6 +324,9 @@ for L in range(10, 15, 5):
         # ax.plot(sol_E, sol_C, "y*", xnew, ynew, "b--")
         ax.plot(sol_C, sol_E, "b*")
         ax.plot(xnew, ynew, "b--", alpha = 0.2)
+
+        eps_range = np.arange(eps_MIN,eps_END+eps_step,eps_step)
+            
         # ax.set_xticks(sol_C)
         # ax.set_yticks(sol_E)
 
@@ -580,15 +339,15 @@ for L in range(10, 15, 5):
         ax.yaxis.set_minor_locator(AutoMinorLocator())
 
         #Clona eixo y
-        ax2 = ax.twinx()
+        # ax2 = ax.twinx()
 
         #Configura eixo secundario
-        ax2.plot(sol_C,np.arange(eps_MIN,eps_END+eps_step,eps_step), alpha=0)
-        ax2.set_yticks(np.arange(eps_MIN,eps_END+eps_step,eps_step))
-        ax2.get_xaxis().set_visible(False)
-        ax2.set_ylabel("$\\epsilon$ (points)")
-        ax2.yaxis.label.set_color('blue')
-        ax2.tick_params(axis='y', colors='blue')
+        # ax2.plot(sol_C,np.arange(eps_MIN,eps_END+eps_step,eps_step), alpha=0)
+        # ax2.set_yticks(np.arange(eps_MIN,eps_END+eps_step,eps_step))
+        # ax2.get_xaxis().set_visible(False)
+        # ax2.set_ylabel("$\\epsilon$ (points)")
+        # ax2.yaxis.label.set_color('blue')
+        # ax2.tick_params(axis='y', colors='blue')
 
         # ax2.yaxis.set_minor_locator(AutoMinorLocator())
         
